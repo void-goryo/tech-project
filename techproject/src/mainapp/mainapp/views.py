55420ea00065070672e2b0eb85603f786a2b4879
Gabriel <GABEHJ2001@GMAIL.COM>
2022-04-08 02:16:43 -0700

    django part 10

--- techproject/src/mainapp/mainapp/views.py
+++ techproject/src/mainapp/mainapp/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    products = ["Cherries","Apples"]
+    products = ['Cherries', 'Apples', 'Oranges', 'Strawberrys', 'Pears', 'Watermelon']
     context = {
-        'products': products
+        'products': products,
     }
     return render (request, 'home.html', context)
