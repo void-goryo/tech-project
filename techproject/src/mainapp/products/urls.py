9ab19d897fe934c3a1626e5153b03e0f76eaaaff
Gabriel <GABEHJ2001@GMAIL.COM>
2022-04-14 12:26:31 -0700

    Done

--- techproject/src/mainapp/products/urls.py
+++ techproject/src/mainapp/products/urls.py
@@ -8,5 +8,8 @@ from .import views
 urlpatterns = [
     path('admin_console', views.admin_console, name='admin_console'),
     path('<int:pk>/details/', views.details, name='details'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('confirmDelete/', views.confirmed, name='confirmed'),
+    path('createRecord/', views.createRecord, name='createRecord'),
     
 ]
--- techproject/src/mainapp/products/views.py
+++ techproject/src/mainapp/products/views.py
@@ -14,7 +14,7 @@ def details(request, pk):
     item = get_object_or_404(Product, pk=pk)
     form = ProductForm(data=request.POST or None, instance=item)
     if request.method == 'POST':
-        if form.is_valid() == 'POST':
+        if form.is_valid():
             form2 = form.save(commit=False)
             form2.save()
             return redirect('admin_console')
@@ -31,3 +31,27 @@ def delete(request, pk):
         return redirect('admin_console')
     context = {'item': item,}
     return render(request, 'products/confirmDelete.html', context)
+
+
+def confirmed(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('admin_console')
+    else:
+        return redirect('admin_console')
+
+
+def createRecord(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('admin_console')
+    else:
+        print(form.errors)
+        form = ProductForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'products/createRecord.html', context)
